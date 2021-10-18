2d9fae0cc434bfdfca807e05272ac8de1029e051
MartinSkachkov <81876862+MartinSkachkov@users.noreply.github.com>
2021-10-18 21:50:43 +0300

    finds the sum of two numbers
    Write a program in C++ to print the sum of two numbers.

---
+++ w3resourceEx/Basic/Sum.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	double num1, num2;
+	cout << "Enter first num:";
+	cin >> num1;
+	cout << "Enter second num:";
+	cin >> num2;
+	cout << (num1 + num2) << endl;
+
+	return 0;
+}
