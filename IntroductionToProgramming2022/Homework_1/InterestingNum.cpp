00d383dbf3507095a342b5fa089bc1eb8596c67e
MartinSkachkov <81876862+MartinSkachkov@users.noreply.github.com>
2022-07-22 11:11:08 +0300

    task_3

---
+++ IntroductionToProgramming2022/Homework_1/InterestingNum.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	//валидация на входа за интервала [a,b]
+	int a, b;
+	try {
+		cout << "A: ";
+		cin >> a;
+		cout << "B: ";
+		cin >> b;
+		if (a > b) {
+			throw "Invalid interval!";
+		}
+	}
+	catch (const char* err) {
+		cout << err;
+	}
+
+	int sum = 0; // сумата от всички интересни числа в [a,b]
+	//проверяваме дали числото е интересно
+	for (size_t number = a; number <= b; number++) { //'а' ни е началото на интервала, а 'b' е край
+		bool isInteresting = true;
+		int currentNum = number; //currNum = 94
+
+		while (currentNum != 0) {
+			//случай за примерен интервал [5,8]
+			if (currentNum <= 10) {
+				break;
+			}
+
+			int lastDigit = (currentNum % 10) % 2; //(94 % 10)%2 --> (4)%2 --> 0
+			int previousDigit = ((currentNum / 10) % 10) % 2; //(94 / 10 % 10)%2 --> (9 % 10)%2 --> (9)%2 --> 1;
+			if (lastDigit == previousDigit) {
+				isInteresting = false;
+				break;
+			}
+			currentNum /= 10;
+		}
+		if (isInteresting) {
+			sum += number;
+			cout << number << ' ';
+		}
+	}
+	cout << endl << "Sum of the interesting numbers is: " << sum << endl;
+
+	return 0;
+}
